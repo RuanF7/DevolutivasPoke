7d134f545ec31e4ad1fe3ed8cfb62e2c11da2ccc
RuanF7 <ruan.correia@kukac.com.br>
2024-05-27 18:46:47 -0300

    REFACTOR: Adding LoginPageForm

--- src/app/components/layout/Layout.tsx
+++ src/app/components/layout/Layout.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 import Head from 'next/head';
+import LoginPageForm from '../forms/loginPageForm/LoginPageForm';
 
 const Layout = () => {
   return (
@@ -12,16 +13,21 @@ const Layout = () => {
       </Head>
       <Header />
       <div className="flex items-center justify-center min-h-screen">
-        <Image
-          src="/pokedex.jpg"
-          alt="Background Image"
-          width={400}
-          height={400}
-          objectFit="cover"
-          objectPosition="top right"
-          priority
-          className="z-0"
-        />
+        <div className="relative">
+          <Image
+            src="/pokedex.jpg"
+            alt="Background Image"
+            width={400}
+            height={400}
+            objectFit="cover"
+            objectPosition="top right"
+            priority
+            className="z-0"
+          />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <LoginPageForm />
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
