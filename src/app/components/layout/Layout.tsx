0ef994b27798fcb35a9485ba5005a9d8a2bf0b1d
RuanF7 <ruan.correia@kukac.com.br>
2024-05-27 18:16:33 -0300

    REFACTOR: Layout changes, Header, Footer and a BG Image

--- src/app/components/layout/Layout.tsx
+++ src/app/components/layout/Layout.tsx
@@ -1,11 +1,28 @@
+import Image from 'next/image';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
+import Head from 'next/head';
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div>
+      <Head>
+        <link rel="shortcut icon" href="/images/pokeball.png" />
+        <title>PokeDevolutivas</title>
+      </Head>
       <Header />
-      <main className="flex-grow">{children}</main>
+      <div className="flex items-center justify-center min-h-screen">
+        <Image
+          src="/pokedex.jpg"
+          alt="Background Image"
+          width={400}
+          height={400}
+          objectFit="cover"
+          objectPosition="top right"
+          priority
+          className="z-0"
+        />
+      </div>
       <Footer />
     </div>
   );
