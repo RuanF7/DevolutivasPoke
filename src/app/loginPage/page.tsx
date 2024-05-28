0de1eb1ba948d248708f66420796bfc81a7adaed
RuanF7 <ruan.correia@kukac.com.br>
2024-05-28 10:38:56 -0300

    BUILD: Change all pages 'cause of new nextjs pages configuration

---
+++ src/app/loginPage/page.tsx
@@ -0,0 +1,13 @@
+export default function LoginPage() {
+  return (
+    <>
+      <h1>Login</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
+        deleniti dolorum nostrum, hic minus mollitia maiores qui neque voluptas
+        suscipit recusandae veniam, repellendus eveniet consequuntur reiciendis
+        velit cum! Cumque, saepe.
+      </p>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
 import Layout from './components/layout/Layout';
 
 export default function Home() {
   return (
     <main>
       <Layout />
+      <Link href="/loginPage">LoginPage</Link>
     </main>
   );
 }
