a7048e03ad21e9cb2b843a03e60de7946cf9b326
RuanF7 <ruan.correia@kukac.com.br>
2024-05-28 14:21:52 -0300

    FEAT: Calling LayoutRegister Component

---
+++ src/app/loginPage/register/page.tsx
@@ -0,0 +1,9 @@
+import LayoutRegister from '@/app/components/layout/layoutRegister/LayoutRegister';
+
+export default function Register() {
+  return (
+    <>
+      <LayoutRegister />
+    </>
+  );
+}
