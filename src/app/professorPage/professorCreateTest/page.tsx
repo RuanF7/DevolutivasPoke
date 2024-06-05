0eb65550c4af2313b864e74d9564bf8845df3b96
RuanF7 <ruan.correia@kukac.com.br>
2024-06-05 19:28:20 -0300

    FEAT: Add ProfessorCreateTest component and page

---
+++ src/app/professorPage/professorCreateTest/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LayoutProfessorCreateTest from '@/app/components/layout/layoutProfessorPage/layoutProfessorCreateTest/LayoutProfessorCreateTest';
+
+const ProfessorCreateTestPage = () => {
+  return (
+    <>
+      <LayoutProfessorCreateTest />
+    </>
+  );
+};
+
+export default ProfessorCreateTestPage;
