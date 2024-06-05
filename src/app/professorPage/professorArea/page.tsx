ffd9317d1b94bc6769636d97992a8fbaa3a1b282
RuanF7 <ruan.correia@kukac.com.br>
2024-06-05 19:26:30 -0300

    REFACTOR: professor page components and import paths

---
+++ src/app/professorPage/professorArea/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LayoutProfessorPage from '@/app/components/layout/layoutProfessorPage/LayoutProfessorPage';
+
+const ProfessorPage = () => {
+  return (
+    <>
+      <LayoutProfessorPage />
+    </>
+  );
+};
+
+export default ProfessorPage;
