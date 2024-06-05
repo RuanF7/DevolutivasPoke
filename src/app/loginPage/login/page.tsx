6faf12771f8067bf01ec7417a6c806aa5284dcc6
RuanF7 <ruan.correia@kukac.com.br>
2024-06-05 16:00:40 -0300

    REFACTOR: Update login page to use LayoutLogin component

--- src/app/loginPage/login/page.tsx
+++ src/app/loginPage/login/page.tsx
@@ -1,13 +1,9 @@
+import LayoutLogin from '@/app/components/layout/layoutLogin/LayoutLogin';
+
 export default function Login() {
   return (
     <>
-      <h1>Login</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
-        deleniti dolorum nostrum, hic minus mollitia maiores qui neque voluptas
-        suscipit recusandae veniam, repellendus eveniet consequuntur reiciendis
-        velit cum! Cumque, saepe.
-      </p>
+      <LayoutLogin />
     </>
   );
 }
