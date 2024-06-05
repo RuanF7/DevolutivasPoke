30f941c7d0abd9420692766a281990bb55032e08
RuanF7 <ruan.correia@kukac.com.br>
2024-06-05 19:22:21 -0300

    FEAT: Add LayoutProfessorCorrectTest component for professor correct test page

---
+++ src/app/components/layout/layoutProfessorPage/layoutProfessorCorrectTest/LayoutProfessorCorrectTest.tsx
@@ -0,0 +1,39 @@
+// components/layout/layoutProfessorPage/layoutProfessorCorrectTest/LayoutProfessorCorrectTest.tsx
+import React from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import Header from '@/app/components/header/Header';
+import Footer from '@/app/components/footer/Footer';
+import ProfessorCorrectTest from '@/app/professorPage/professorCorrectTest/ProfessorCorrectTest';
+
+const LayoutProfessorCorrectTest: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <link rel="shortcut icon" href="/images/pokeball.png" />
+        <title>PokeDevolutivas</title>
+      </Head>
+      <Header />
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="relative">
+          <Image
+            src="/pokedex.jpg"
+            alt="Background Image"
+            width={400}
+            height={400}
+            objectFit="cover"
+            objectPosition="top right"
+            priority
+            className="z-0"
+          />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <ProfessorCorrectTest />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default LayoutProfessorCorrectTest;
