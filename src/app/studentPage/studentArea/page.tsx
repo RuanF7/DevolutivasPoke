33dd0179e8af1055229fe6656f9cf03ba0b4c15a
RuanF7 <ruan.correia@kukac.com.br>
2024-06-05 19:30:38 -0300

    FEAT: Add StudentAreaPage component for student area page

---
+++ src/app/studentPage/studentArea/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LayoutStudentArea from '@/app/components/layout/layoutStudentPage/layoutStudentArea/LayoutStudentArea';
+
+const StudentAreaPage = () => {
+  return (
+    <>
+      <LayoutStudentArea />
+    </>
+  );
+};
+
+export default StudentAreaPage;
---
+++ src/app/studentPage/studentTest/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LayoutStudentTest from '@/app/components/layout/layoutStudentPage/layoutStudentTest/LayoutStudentTest';
+const StudentTestPage = () => {
+  return (
+    <>
+      <LayoutStudentTest />
+    </>
+  );
+};
+
+export default StudentTestPage;
