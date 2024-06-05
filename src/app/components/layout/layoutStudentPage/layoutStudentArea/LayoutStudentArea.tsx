5a5adbcf01745a33af757fe7a1b386010b7bc58d
RuanF7 <ruan.correia@kukac.com.br>
2024-06-05 19:24:01 -0300

    FEAT: Add LayoutStudentAreaPage component for student area page

---
+++ src/app/components/layout/layoutStudentPage/layoutStudentArea/LayoutStudentArea.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import Header from '@/app/components/header/Header';
+import Footer from '@/app/components/footer/Footer';
+import StudentArea from '@/app/studentPage/studentArea/studentArea';
+
+const LayoutStudentAreaPage: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <link rel="shortcut icon" href="/images/pokeball.png" />
+        <title>PokeDevolutivas</title>
+      </Head>
+      <Header />
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="relative">
+          <Image
+            src="/pokedex.jpg"
+            alt="Background Image"
+            width={400}
+            height={400}
+            objectFit="cover"
+            objectPosition="top right"
+            priority
+            className="z-0"
+          />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <StudentArea />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default LayoutStudentAreaPage;
