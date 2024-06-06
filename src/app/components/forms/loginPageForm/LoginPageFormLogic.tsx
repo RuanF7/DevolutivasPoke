77f26c4abeff357f2136dd1bb109636c8becd792
RuanF7 <ruanfinamor@gmail.com>
2024-06-06 17:29:55 -0300

    FEAT: Add handleToken function for decoding and handling JWT token

--- src/app/components/forms/loginPageForm/LoginPageFormLogic.tsx
+++ src/app/components/forms/loginPageForm/LoginPageFormLogic.tsx
@@ -2,13 +2,19 @@
 import React, { useState } from 'react';
 import LoginPageForm from './LoginPageForm';
 import { useRouter } from 'next/navigation';
+import jwt from 'jsonwebtoken';
+
+interface Token extends jwt.JwtPayload {
+  userId: number;
+  isProfessor: boolean;
+}
 
 const LoginPageLogic: React.FC = () => {
   const [formData, setFormData] = useState({
     email: '',
     senha: ''
   });
-
+  const [isProfessor, setIsProfessor] = useState(false);
   const router = useRouter();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,8 +25,33 @@ const LoginPageLogic: React.FC = () => {
     }));
   };
 
+  const handleToken = (token: string): boolean => {
+    try {
+      console.log('Token:', token);
+      const decodedToken = jwt.verify(token, 'your_jwt_secret') as Token;
+      console.log('Decoded token:', decodedToken);
+
+      const userId = (decodedToken as Token).userId;
+      const isProfessor = (decodedToken as Token).isProfessor;
+
+      localStorage.setItem('userId', userId.toString());
+      localStorage.setItem('isProfessor', isProfessor.toString());
+
+      setIsProfessor(isProfessor);
+      return isProfessor;
+    } catch (error) {
+      console.error('Error:', error);
+      return false;
+    }
+  };
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (!formData.email || !formData.senha) {
+      throw new Error('Email and password are required');
+    }
+
     try {
       const response = await fetch('http://localhost:3000/api/auth/login', {
         method: 'POST',
@@ -31,12 +62,14 @@ const LoginPageLogic: React.FC = () => {
       });
 
       const result = await response.json();
-      if (response.ok) {
-        console.log('User logged in', result);
-        localStorage.setItem('token', result.token);
-        router.push('/studentPage/studentArea');
+      if (!response.ok) {
+        throw new Error(result.message);
+      }
+      const isProfessor = handleToken(result.token);
+      if (isProfessor) {
+        router.push('/professorPage/professorArea');
       } else {
-        console.error('Error:', result.error);
+        router.push('/studentPage/studentArea');
       }
     } catch (error) {
       console.error('Error:', error);
