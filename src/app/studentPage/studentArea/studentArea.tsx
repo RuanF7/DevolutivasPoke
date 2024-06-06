efa5e02c742fd62138c29b696123ddf89543131d
RuanF7 <ruanfinamor@gmail.com>
2024-06-06 13:25:37 -0300

    FEAT: Update StudentArea component with navigation functionality

--- src/app/studentPage/studentArea/studentArea.tsx
+++ src/app/studentPage/studentArea/studentArea.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+'use client';
+import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 
 const StudentArea: React.FC = () => {
+  const router = useRouter();
+
+  const handleNavigation = (path: string) => {
+    router.push(path);
+  };
+
+  // const handleMatricular = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       'http://localhost:3000/api/aluno/matricular',
+  //       {
+  //         method: 'POST',
+  //         headers: {
+  //           'Content-Type': 'application/json'
+  //         },
+  //         body: JSON.stringify(matriculaData)
+  //       }
+  //     );
+  //     const result = await response.json();
+  //     if (response.ok) {
+  //       console.log('Aluno matriculado', result);
+  //     } else {
+  //       console.error('Erro ao matricular aluno:', result.error);
+  //     }
+  //   } catch (error) {
+  //     console.error('Erro:', error);
+  //   }
+  // };
+
+  // const handleRealizarProva = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       'http://localhost:3000/api/aluno/realizar-prova',
+  //       {
+  //         method: 'POST',
+  //         headers: {
+  //           'Content-Type': 'application/json'
+  //         },
+  //         body: JSON.stringify(provaData)
+  //       }
+  //     );
+  //     const result = await response.json();
+  //     if (response.ok) {
+  //       console.log('Prova realizada', result);
+  //     } else {
+  //       console.error('Erro ao realizar prova:', result.error);
+  //     }
+  //   } catch (error) {
+  //     console.error('Erro:', error);
+  //   }
+  // };
+
   return (
     <div className="w-64 h-screen flex justify-center items-center">
       <aside className="bg-white w-full max-w-md rounded-xl bg-opacity-5 shadow-lg shadow-gray-900 p-4">
@@ -9,7 +62,7 @@ const StudentArea: React.FC = () => {
           <div className="mr-4">
             <Image
               src="/aluno1.png"
-              alt="Professor Image"
+              alt="Student Image"
               width={90}
               height={100}
               objectFit="cover"
@@ -17,13 +70,24 @@ const StudentArea: React.FC = () => {
             />
           </div>
           <div className="flex flex-col justify-center">
-            <button className="w-full py-2 my-2 bg-gray-800 text-white rounded hover:bg-gray-900">
+            <button
+              onClick={() =>
+                handleNavigation('/studentPage/studentCourseRegistration')
+              }
+              className="w-full py-2 my-2 bg-gray-800 text-white rounded hover:bg-gray-900"
+            >
               Matricular
             </button>
-            <button className="w-full py-2 my-2 bg-gray-800 text-white rounded hover:bg-gray-900">
+            <button
+              onClick={() => handleNavigation('/studentPage/studentTest')}
+              className="w-full py-2 my-2 bg-gray-800 text-white rounded hover:bg-gray-900"
+            >
               Fazer Prova
             </button>
-            <button className="w-full py-2 my-2 bg-gray-800 text-white rounded hover:bg-gray-900">
+            <button
+              className="w-full py-2 my-2 bg-gray-800 text-white rounded hover:bg-gray-900"
+              onClick={() => handleNavigation('/studentPage/studentFeedback')}
+            >
               Devolutivas
             </button>
           </div>
