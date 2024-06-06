1a00f90e383aae979a0cdac8ceb91cc89b419d30
RuanF7 <ruanfinamor@gmail.com>
2024-06-06 13:26:14 -0300

    FEAT: Add course registration functionality to StudentCourseRegistration component

--- src/app/studentPage/studentCourseRegistration/StudentCourseRegistration.tsx
+++ src/app/studentPage/studentCourseRegistration/StudentCourseRegistration.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Image from 'next/image';
 
 const StudentCourseRegistration: React.FC = () => {
+  const [cursoNome, setCursoNome] = useState('');
+
+  const handleCursoNomeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCursoNome(e.target.value);
+  };
+
+  const handleMatricularClick = async () => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      console.error('Token não encontrado. Faça o login primeiro.');
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        'http://localhost:3000/api/aluno/matricular',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+          },
+          body: JSON.stringify({
+            cursoNome: cursoNome
+          })
+        }
+      );
+
+      const result = await response.json();
+      if (response.ok) {
+        console.log('Aluno matriculado com sucesso', result);
+      } else {
+        console.error('Erro ao matricular aluno:', result.error);
+      }
+    } catch (error) {
+      console.error('Erro:', error);
+    }
+  };
+
   return (
     <div className="w-64 h-screen flex justify-center items-center">
       <aside className="bg-white w-full max-w-md rounded-xl bg-opacity-5 shadow-lg shadow-gray-900 p-4">
@@ -21,10 +61,15 @@ const StudentCourseRegistration: React.FC = () => {
               <input
                 type="text"
                 placeholder="Nome do Curso"
+                value={cursoNome}
+                onChange={handleCursoNomeChange}
                 className="w-full py-2 px-3 mb-3 bg-gray-100 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
               />
             </div>
-            <button className="w-full py-2 my-2 bg-gray-800 text-white rounded hover:bg-gray-900">
+            <button
+              onClick={handleMatricularClick}
+              className="w-full py-2 my-2 bg-gray-800 text-white rounded hover:bg-gray-900"
+            >
               Matricular
             </button>
           </div>
