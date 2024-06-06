3fed1dcb123662e9af00e0a5fa30d8809adb221d
RuanF7 <ruanfinamor@gmail.com>
2024-06-06 17:30:58 -0300

    FEAT: Add navigation functionality to ProfessorArea component

--- src/app/professorPage/professorArea/professorArea.tsx
+++ src/app/professorPage/professorArea/professorArea.tsx
@@ -1,7 +1,15 @@
+'use client';
 import React from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 const ProfessorArea = () => {
+  const router = useRouter();
+
+  const handleNavigation = (path: string) => {
+    router.push(path);
+  };
+
   return (
     <div className="w-64 h-screen flex justify-center items-center">
       <aside className="bg-white w-full max-w-md rounded-xl bg-opacity-5 shadow-lg shadow-gray-900 p-4">
@@ -17,13 +25,28 @@ const ProfessorArea = () => {
             />
           </div>
           <div className="flex flex-col justify-center">
-            <button className="w-full py-2 my-2 bg-gray-800 text-white rounded hover:bg-gray-900">
+            <button
+              onClick={() =>
+                handleNavigation('/professorPage/professorBackpack')
+              }
+              className="w-full py-2 my-2 bg-gray-800 text-white rounded hover:bg-gray-900"
+            >
               Adicionar Pokémon
             </button>
-            <button className="w-full py-2 my-2 bg-gray-800 text-white rounded hover:bg-gray-900">
+            <button
+              onClick={() =>
+                handleNavigation('/professorPage/professorCreateTest')
+              }
+              className="w-full py-2 my-2 bg-gray-800 text-white rounded hover:bg-gray-900"
+            >
               Criar Prova
             </button>
-            <button className="w-full py-2 my-2 bg-gray-800 text-white rounded hover:bg-gray-900">
+            <button
+              onClick={() =>
+                handleNavigation('/professorPage/professorCorrectTest')
+              }
+              className="w-full py-2 my-2 bg-gray-800 text-white rounded hover:bg-gray-900"
+            >
               Corrigir Prova
             </button>
           </div>
