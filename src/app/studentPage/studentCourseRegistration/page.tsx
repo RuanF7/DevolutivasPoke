1e30544d35870981897aa9bda948e44ec12ebce7
RuanF7 <ruanfinamor@gmail.com>
2024-06-06 09:30:46 -0300

    FEAT: Add StudentCourseRegistrationPage component for student course registration page

---
+++ src/app/studentPage/studentCourseRegistration/page.tsx
@@ -0,0 +1,12 @@
+import LayoutStudentCourseRegistration from '@/app/components/layout/layoutStudentPage/layoutStudentCourseRegistration/LayoutStudentCourseRegistration';
+import React from 'react';
+
+const StudentCourseRegistrationPage = () => {
+  return (
+    <>
+      <LayoutStudentCourseRegistration />
+    </>
+  );
+};
+
+export default StudentCourseRegistrationPage;
---
+++ src/app/studentPage/studentFeedback/page.tsx
@@ -0,0 +1,12 @@
+import LayoutStudentFeedBack from '@/app/components/layout/layoutStudentPage/layoutStudentFeedBack/LayoutStudentFeedBack';
+import React from 'react';
+
+const StudentFeedBackPage = () => {
+  return (
+    <>
+      <LayoutStudentFeedBack />
+    </>
+  );
+};
+
+export default StudentFeedBackPage;
