071076ee756e6ee477ea8b4770f892f551e0b7d8
RuanF7 <ruanfinamor@gmail.com>
2024-06-18 09:06:16 -0300

    FEAT: Professor create course new layout

---
+++ src/app/components/layout/layoutProfessorPage/layoutProfessorCreateCourse/LayoutProfessorCreateCourse.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import HeaderProfessor from '@/app/components/header/HeaderProfessor';
+import Footer from '@/app/components/footer/Footer';
+import ProfessorCreateCourse from '@/app/professorPage/professorCreateCourse/ProfessorCreateCourse';
+
+const LayoutProfessorCreateCourse: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <link rel="shortcut icon" href="/images/pokeball.png" />
+        <title>PokeDevolutivas</title>
+      </Head>
+      <HeaderProfessor />
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="relative">
+          <Image
+            src="/pokedex.jpg"
+            alt="Background Image"
+            width={400}
+            height={400}
+            objectFit="cover"
+            objectPosition="top right"
+            priority
+            className="z-0"
+          />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <ProfessorCreateCourse />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default LayoutProfessorCreateCourse;
